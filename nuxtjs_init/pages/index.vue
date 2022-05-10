5c9e2de5ad5f81b1722c9b49e43bed0b92af6ba8
yongsungHeo <ysheo94@gmail.com>
2022-05-10 21:19:39 +0900

    Using asyncData sample is added.

--- nuxtjs_init/pages/index.vue
+++ nuxtjs_init/pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-    <Tutorial />
+    <!-- <Tutorial /> -->
+    <div>
+        {{ products }}
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'IndexPage',
+    async asyncData() {
+        const res = await axios.get('http://localhost:3000/products');
+        const products = res.data;
+        return { products }
+    },
 }
 </script>
