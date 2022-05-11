3ee9884b87d3cad66d15b3cff36d0c9b530c6655
yongsungHeo <ysheo94@gmail.com>
2022-05-11 19:26:05 +0900

    Default layout is added.

---
+++ nuxtjs_init/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            <NuxtLink to="/">Nuxt Init</NuxtLink>
+        </h1>
+        <Nuxt></Nuxt>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DefaultPage',
+}
+</script>
+
+<style>
+</style>
